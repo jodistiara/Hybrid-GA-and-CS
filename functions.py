bf5d8d6c73553044fc1798348b6692547f372b8e
jodistiara <jodistiara30@gmail.com>
2019-02-07 02:23:07 +0700

    function has been added for trial purpose

--- functions.py
+++ functions.py
@@ -4,14 +4,14 @@
 #import file ke dataframe
 
 #scoring hard constraints
-def hard_constrains(egg)
+# def hard_constrains(egg)
 
 
-#cek ruangan
-def hard1(id):
-    score = 0
-    for i in range(len(id)):
-        if id[i]
+# #cek ruangan
+# def hard1(id):
+#     score = 0
+#     for i in range(len(id)):
+#         if id[i]
 #cek dosen
 
 #cek matkul nabrak apa enggak
@@ -22,5 +22,10 @@ def hard1(id):
 #cek tiap aturan pilihan
 
 #==============================================================
+import numpy as np
 
-
+coef = np.array([30, 4, 67, 23, 99])
+def fitness(array):
+        score = np.dot(array, coef)
+        value = float(1 / (2000 - score))
+        return value
