03d5f21e502c4b1e3cd3ad4078fc5c208c1f74dd
jodistiara <jodistiara30@gmail.com>
2019-02-07 02:21:42 +0700

    all processes have been writen except for cuckoo search

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-import config as cf
-from individual import Cuckooegg as egg
-from individual import Chromosome as cr 
-import function as fn 
+from config import Config as cf
+import individu as idv
+# import function as fn 
 
 import numpy as np
 
+def main():
+    pops = []
+    best = []
+    #================create initial population=============
+    # num of id: cf.get_popsize()
+    for id in range(cf.get_popsize()): pops.append(idv.Individu())
+    
+    #================calculate fitness=====================
+    pops = sorted(pops, reverse=True, key=lambda ID: ID.get_fitness())
+    current_best = pops[0].get_fitness()
+    
+    print(pops[0])
+    print(current_best)
+
+    for t in range(cf.get_maxgen()):
+        #================crossover==========================
+        select_from_pc, num = idv.select_individuals(pops, cf.get_Pc())
+        selection = idv.selection(select_from_pc, cf.get_pointer())
+        new_pops = idv.crossover(selection)
+
+        #=======================mutation====================
+        select_from_pm, num = idv.select_individuals
+        new_pops = idv.mutation(new_pops, num)
+
+
+        #====================cuckoo search==================
+
+
+        #=============generational replacement==============
+        pops = idv.replacement(new_pops)
+
+main()
