6c01b967ff7019f8f0ad4f109c00d1efcfcadcc1
jodistiara <jodistiara30@gmail.com>
2019-03-10 22:47:56 +0700

    added function to write genes and revised crossover processes

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 15:42:36 2019
+
+@author: cdjodistiara
+"""
+
 from config import Config as cf
 import individu as idv
+import random as rand
 # import function as fx 
 
-import numpy as np
+
+def show_all(pops):
+  for i in range(len(pops)):
+    print("[", i, "] -> ", pops[i].get_allel(), " - f: ", pops[i].get_fitness())
 
 def main():
     pops = []
-    best = []
+#    best = []
     #================create initial population=============
     # num of id: cf.get_popsize()
     for id in range(cf.get_popsize()): pops.append(idv.Individu())
     
+    print("initial population: ")
+    show_all(pops)
     #================calculate fitness=====================
-    pops = sorted(pops, reverse=True, key=lambda ID: ID.get_fitness())
+#    pops = sorted(pops, reverse=True, key=lambda ID: ID.get_fitness())
 #     current_best = pops[0].get_fitness()
     
 #     print(pops[0])
@@ -21,31 +35,42 @@ def main():
     for t in range(cf.get_maxgen()):
         print("===============GEN ", t, "================")
         #================crossover==========================
-        select_from_pc, num = idv.select_individuals(pops, cf.get_Pc())
-        selection = idv.selection(select_from_pc)
-        new_pops = idv.crossover(selection)
-
-        #=======================mutation====================
-        select_from_pm, num = idv.select_individuals(new_pops, cf.get_Pm())
-        for index in num: new_pops[index].mutate()
-
-        #====================cuckoo search==================
-        #create new egg from best individual
-        new_pops = sorted(new_pops, reverse=True, key=lambda ID: ID.get_fitness())
-        allel, fitness = new_pops[0].new_egg()
-
-        #check random egg then review the fitness value
-        r = rand.randint(0, len(new_pops)-1)
-        if fitness > new_pops[r].get_fitness():
+        select = idv.selection(pops)
+        print("selected from RWS: ")
+        show_all(select)
+        selected, num = idv.select_individuals(select, cf.get_Pc())
+        print("selected from Pc: ", num)
+        show_all(selected)
+        if num:
+            new_pops = idv.crossover(selected)
+            print("offsprings: ")
+            show_all(new_pops)
+            
+            #=======================mutation====================
+            select_from_pm, num2 = idv.select_individuals(new_pops, cf.get_Pm())
+            for index in num2: new_pops[index].mutate()
+    
+            #====================cuckoo search==================
+            #create new egg from best individual
+            new_pops = sorted(new_pops, reverse=True, key=lambda ID: ID.get_fitness())
+            print("best: ", new_pops[0].get_allel())
+            allel, fitness = new_pops[0].new_egg()
+            print("cuckoo egg: ", allel)
+    
+            #check random egg then review the fitness value
+            r = rand.randint(0, len(new_pops)-1)
+            print("chosen individual: ", new_pops[r].get_allel())
+            if fitness > new_pops[r].get_fitness():
                 new_pops[r].set_allel(allel)
                 new_pops[r].set_fitness(fitness)
-
-        new_pops = sorted(new_pops, reverse=True, key=lambda ID: ID.get_fitness())
+            
+            #abandoned egg get replaced
+            new_pops = idv.abandon_egg(new_pops)
+            
+            #=============generational replacement==============
+            pops = idv.replacement(pops, new_pops)
         
-        #abandoned egg get replaced
-        new_pops = idv.abandon_egg(new_pops)
-        
-        #=============generational replacement==============
-        pops = idv.replacement(new_pops)
+        print("new pops: ")
+        show_all(pops)
 
 main()
